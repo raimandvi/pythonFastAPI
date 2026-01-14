125d953373566b0ec317aba71a29d8392f07417d
raimandvi <raimandvi123@gmail.com>
2026-01-14 14:19:17 +0530

    Added query parameters and request body examples

--- main.py
+++ main.py
@@ -5,3 +5,21 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "FastAPI is working"}
+
+
+@app.get("/add")
+def add_num(a: int, b:int):
+    return {"result": a + b}
+
+# Request Body (Pydantic Model)
+
+from pydantic import BaseModel
+
+class Student(BaseModel):
+    name: str
+    age: int
+    marks: float
+
+@app.post("/student")
+def create_student(student: Student):
+    return student
